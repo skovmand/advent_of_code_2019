c12e2157913f0856e5688d47d0350dcc467ed84a
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2019-12-04 11:19:09 +0100

    Tests for part 2

--- test/03_crossed_wires_test.exs
+++ test/03_crossed_wires_test.exs
@@ -30,4 +30,28 @@ defmodule AdventOfCode2019.CrossedWiresTest do
       assert @wire_paths |> CrossedWires.closest_distance() == 1519
     end
   end
+
+  describe "part 2: the intersection with fewest steps" do
+    test "example 1" do
+      input = """
+      R75,D30,R83,U83,L12,D49,R71,U7,L72
+      U62,R66,U55,R34,D71,R55,D58,R83
+      """
+
+      assert input |> CrossedWires.fewest_steps() == 610
+    end
+
+    test "example 2" do
+      input = """
+      R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+      U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+      """
+
+      assert input |> CrossedWires.fewest_steps() == 410
+    end
+
+    test "the actual closest intersection" do
+      assert @wire_paths |> CrossedWires.fewest_steps() == 0
+    end
+  end
 end
