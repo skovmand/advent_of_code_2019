022c82e0f61aa4037fbea95d409c4b962b9694a3
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2019-12-04 19:18:08 +0100

    Refactoring the solution

--- lib/advent_of_code_2019/04_password.ex
+++ lib/advent_of_code_2019/04_password.ex
@@ -1,28 +1,35 @@
 defmodule AdventOfCode2019.Password do
-  @doc """
+  @moduledoc """
+  Advent of Code 2019 day 4: Finding candidates for the forgotten password
   """
-  def find_candidates(from, to) do
+
+  @doc """
+  Find password candidates in a given range
+  """
+  def find_candidates(from, to, :basic) do
+    do_find_candidates(from, to, &digit_list_is_password?/1)
+  end
+
+  def find_candidates(from, to, :extended) do
+    do_find_candidates(from, to, &digit_list_is_password_extended?/1)
+  end
+
+  defp do_find_candidates(from, to, candidate_fn) do
     from..to
-    |> Enum.filter(&is_password_candidate?/1)
+    |> Enum.map(&number_to_digit_list/1)
+    |> Enum.filter(candidate_fn)
     |> Enum.count()
   end
 
   @doc """
-  Test if a number is a password candidate
+  Check whether a single password is a candidate
   """
-  def is_password_candidate?(number) do
+  def is_candidate?(number, :basic) do
     number |> number_to_digit_list() |> digit_list_is_password?()
   end
 
-  def number_to_digit_list(number) when number >= 100_000 and number < 1_000_000 do
-    digit_1 = number |> div(100_000)
-    digit_2 = number |> rem(100_000) |> div(10_000)
-    digit_3 = number |> rem(10_000) |> div(1_000)
-    digit_4 = number |> rem(1_000) |> div(100)
-    digit_5 = number |> rem(100) |> div(10)
-    digit_6 = number |> rem(10)
-
-    [digit_1, digit_2, digit_3, digit_4, digit_5, digit_6]
+  def is_candidate?(number, :extended) do
+    number |> number_to_digit_list() |> digit_list_is_password_extended?()
   end
 
   @doc """
@@ -30,6 +37,12 @@ defmodule AdventOfCode2019.Password do
   """
   def digit_list_is_password?(digit_list) when is_list(digit_list),
     do: increasing?(digit_list) and two_identical?(digit_list)
+
+  @doc """
+  Test if a list of 6 digits is a password
+  """
+  def digit_list_is_password_extended?(digit_list) when is_list(digit_list),
+    do: increasing?(digit_list) and match_extended_criteria?(digit_list)
 
   @doc """
   Test whether a list of numbers is never decreasing
@@ -49,4 +62,44 @@ defmodule AdventOfCode2019.Password do
   def two_identical?([_, _, _, d, d, _]), do: true
   def two_identical?([_, _, _, _, e, e]), do: true
   def two_identical?(_), do: false
+
+  @doc """
+  Check that two adjacent characters are not part of a larger group
+  Yeah, I know - this got out of hand :-)
+  """
+  def match_extended_criteria?([a, a, a, a, a, a]), do: false
+
+  def match_extended_criteria?([a, a, a, a, a, b]) when a != b, do: false
+  def match_extended_criteria?([b, a, a, a, a, a]) when a != b, do: false
+
+  def match_extended_criteria?([b, a, a, a, a, b]) when a != b, do: false
+  def match_extended_criteria?([b, b, a, a, a, a]) when a != b, do: true
+  def match_extended_criteria?([a, a, a, a, b, b]) when a != b, do: true
+
+  def match_extended_criteria?([a, a, a, b, b, c]) when a != b and b != c, do: true
+  def match_extended_criteria?([a, a, a, c, b, b]) when a != b and b != c, do: true
+  def match_extended_criteria?([c, a, a, a, b, b]) when a != b and b != c, do: true
+  def match_extended_criteria?([b, b, a, a, a, c]) when a != b and b != c, do: true
+  def match_extended_criteria?([b, b, c, a, a, a]) when a != b and b != c, do: true
+  def match_extended_criteria?([c, b, b, a, a, a]) when a != b and b != c, do: true
+
+  def match_extended_criteria?([a, a, b, c, d, e]) when a not in [b, c, d, e], do: true
+  def match_extended_criteria?([a, b, b, c, d, e]) when b not in [a, c, d, e], do: true
+  def match_extended_criteria?([a, b, c, c, d, e]) when c not in [a, b, d, e], do: true
+  def match_extended_criteria?([a, b, c, d, d, e]) when d not in [a, b, c, e], do: true
+  def match_extended_criteria?([a, b, c, d, e, e]) when e not in [a, b, c, d], do: true
+
+  def match_extended_criteria?(_), do: false
+
+  # Converts a number into a list of its digits
+  defp number_to_digit_list(number) when number >= 100_000 and number < 1_000_000 do
+    digit_1 = number |> div(100_000)
+    digit_2 = number |> rem(100_000) |> div(10_000)
+    digit_3 = number |> rem(10_000) |> div(1_000)
+    digit_4 = number |> rem(1_000) |> div(100)
+    digit_5 = number |> rem(100) |> div(10)
+    digit_6 = number |> rem(10)
+
+    [digit_1, digit_2, digit_3, digit_4, digit_5, digit_6]
+  end
 end
--- test/04_password_test.exs
+++ test/04_password_test.exs
@@ -5,19 +5,37 @@ defmodule AdventOfCode2019.PasswordTest do
 
   describe "part 1: password combinations" do
     test "111111" do
-      assert Password.is_password_candidate?(111111) == true
+      assert Password.is_candidate?(111_111, :basic) == true
     end
 
     test "223450" do
-      assert Password.is_password_candidate?(223450) == false
+      assert Password.is_candidate?(223_450, :basic) == false
     end
 
     test "123789" do
-      assert Password.is_password_candidate?(123789) == false
+      assert Password.is_candidate?(123_789, :basic) == false
     end
 
     test "solving the puzzle" do
-      assert Password.find_candidates(172930, 683082) == 1675
+      assert Password.find_candidates(172_930, 683_082, :basic) == 1675
+    end
+  end
+
+  describe "part 2: additional rule" do
+    test "112233" do
+      assert Password.is_candidate?(112_233, :extended) == true
+    end
+
+    test "123444" do
+      assert Password.is_candidate?(123_444, :extended) == false
+    end
+
+    test "111122" do
+      assert Password.is_candidate?(111_122, :extended) == true
+    end
+
+    test "adding an additional rule" do
+      assert Password.find_candidates(172_930, 683_082, :extended) == 1142
     end
   end
 end
