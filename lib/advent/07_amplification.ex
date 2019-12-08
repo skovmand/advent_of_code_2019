fc8e1b2f7b8925691acf3451d65ae5b2148258d3
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2019-12-08 13:21:05 +0100

    Day 7 Part 2

--- lib/advent/07_amplification.ex
+++ lib/advent/07_amplification.ex
@@ -3,33 +3,103 @@ defmodule Advent19.Amplification do
   Send more power to the thrusters to reach Santa in time!
   """
 
-  import Advent19.IntcodeV2, only: [compute_result: 2]
+  alias Advent19.IntcodeV2
+  alias Advent19.IntcodeV7
 
   @doc """
-  Find the optimum phase for amplifier output given a program
+  Part 1
+
+  Find the optimum phase for amplifier output given a program by running all five amplifiers
+  in sequence, feeding 0 as input to the first amp, then connecting outputs and inputs on the
+  following amps. In practice it just runs the same program 5 times with the output of the former
+  as input to the next.
   """
   def optimum_phase(program) do
-    all_phase_combinations()
-    |> Enum.map(&run(program, &1))
+    all_phase_combinations(0..4)
+    |> Enum.map(fn phase_combination -> run_serial(program, phase_combination) end)
     |> Enum.max()
   end
 
   @doc """
-  Run the program on the amplifiers for a given phase combination
+  Run a program with serially connected amps to get the output to thrusters
+
+      O-------O  O-------O  O-------O  O-------O  O-------O
+  0 ->| Amp A |->| Amp B |->| Amp C |->| Amp D |->| Amp E |-> (to thrusters)
+      O-------O  O-------O  O-------O  O-------O  O-------O
   """
-  def run(program, phase_combination) do
+  def run_serial(program, phase_combination) do
     phase_combination
-    |> Enum.reduce([0], fn phase, input_signal -> compute_result(program, [phase | input_signal]) end)
+    |> Enum.reduce([0], fn phase, input_signal -> IntcodeV2.compute_result(program, [phase | input_signal]) end)
     |> List.first()
   end
 
+  @doc """
+  Part 2
+
+  Find the optimum phase for the feedback loop amplifier configuration. This needs a reconfigured
+  Intcode computer that returns on every output (opcode 4) with the output and the current instruction
+  pointer. We need to keep track of the instruction pointers for each amp.
+  """
+  def optimum_feedback_loop_phase(program) do
+    all_phase_combinations(5..9)
+    |> Enum.map(fn phase_combination -> run_feedback(program, phase_combination) end)
+    |> Enum.max()
+  end
+
+  @doc """
+  Run a program with amps in feedback loop to get output to thrusters
+
+        O-------O  O-------O  O-------O  O-------O  O-------O
+  0 -+->| Amp A |->| Amp B |->| Amp C |->| Amp D |->| Amp E |-.
+     |  O-------O  O-------O  O-------O  O-------O  O-------O |
+     |                                                        |
+     '--------------------------------------------------------+
+                                                              |
+                                                              v
+                                                       (to thrusters)
+  """
+  def run_feedback(program, phase_combination) do
+    initial_input = phase_combination |> initial_input()
+
+    Stream.cycle(0..4)
+    |> Enum.reduce_while({initial_input, [], %{}}, fn amp, {initial_inputs, next_input, memory_dump} ->
+      {amp_input, initial_inputs} =
+        case Map.fetch(initial_inputs, amp) do
+          :error -> {next_input, initial_inputs}
+          {:ok, initial_input} -> {initial_input ++ next_input, initial_inputs |> Map.delete(amp)}
+        end
+
+      {next_instruction_pointer, amp_program, _} = Map.get(memory_dump, amp, {0, program, []})
+
+      case IntcodeV7.compute(amp_program, amp_input, [], next_instruction_pointer) do
+        {:halt, _} ->
+          {:halt, memory_dump |> last_value_from_amp_e()}
+
+        {:output, output, {next_instruction, program}} ->
+          {:cont, {initial_inputs, output, memory_dump |> Map.put(amp, {next_instruction, program, output})}}
+      end
+    end)
+  end
+
+  # Create a map of amp => initial_input from a given phase combination
+  defp initial_input(phase_combination) do
+    phase_combination
+    |> Enum.map(&[&1])
+    |> List.update_at(0, &(&1 ++ [0]))
+    |> Enum.with_index()
+    |> Enum.into(%{}, fn {k, v} -> {v, k} end)
+  end
+
+  # When the program ends, we fetch the last output from amp E ("4" in our memory dump map)
+  defp last_value_from_amp_e(%{4 => {_, _, [value]}}), do: value
+
   # All 120 possible phases for the amplifiers
-  defp all_phase_combinations() do
-    for a <- 0..4,
-        b <- 0..4,
-        c <- 0..4,
-        d <- 0..4,
-        e <- 0..4,
+  defp all_phase_combinations(range) do
+    for a <- range,
+        b <- range,
+        c <- range,
+        d <- range,
+        e <- range,
         a not in [b, c, d, e],
         b not in [a, c, d, e],
         c not in [a, b, d, e],
--- test/07_amplification_test.exs
+++ test/07_amplification_test.exs
@@ -13,23 +13,42 @@ defmodule Advent19.AmplificationTest do
     test "Max thruster signal 43210 (from phase setting sequence 4,3,2,1,0)" do
       program = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
 
-      assert program |> Common.integer_list() |> Amplification.run([4,3,2,1,0]) == 43210
+      assert program |> Common.integer_list() |> Amplification.run_serial([4, 3, 2, 1, 0]) == 43210
     end
 
     test "Max thruster signal 54321 (from phase setting sequence 0,1,2,3,4)" do
       program = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
 
-      assert program |> Common.integer_list() |> Amplification.run([0,1,2,3,4]) == 54321
+      assert program |> Common.integer_list() |> Amplification.run_serial([0, 1, 2, 3, 4]) == 54321
     end
 
     test "Max thruster signal 65210 (from phase setting sequence 1,0,4,3,2)" do
       program = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
 
-      assert program |> Common.integer_list() |> Amplification.run([1,0,4,3,2]) == 65210
+      assert program |> Common.integer_list() |> Amplification.run_serial([1, 0, 4, 3, 2]) == 65210
     end
 
     test "part 1 answer" do
       assert @opcode |> Amplification.optimum_phase() == 77500
+    end
+  end
+
+  describe "part 2" do
+    test "Max thruster signal 139629729 (from phase setting sequence 9,8,7,6,5)" do
+      program = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+
+      assert program |> Common.integer_list() |> Amplification.run_feedback([9, 8, 7, 6, 5]) == 139_629_729
+    end
+
+    test "Max thruster signal 18216 (from phase setting sequence 9,7,8,5,6)" do
+      program =
+        "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+
+      assert program |> Common.integer_list() |> Amplification.run_feedback([9, 7, 8, 5, 6]) == 18216
+    end
+
+    test "part 2 answer" do
+      assert @opcode |> Amplification.optimum_feedback_loop_phase() == 22_476_942
     end
   end
 end
