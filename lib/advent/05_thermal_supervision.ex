4f2b8851b792fa2efa86d6618567bd82755c6f1c
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2019-12-13 18:56:53 +0100

    Use general runtime for day 5

---
+++ lib/advent/05_thermal_supervision.ex
@@ -0,0 +1,6 @@
+defmodule Advent19.ThermalSupervision do
+  @moduledoc """
+  Day 5 of Advent of Code 2019: Adding the Thermal Environment Supervision Terminal
+  It runs on the general intcode runtime, see the test file
+  """
+end
